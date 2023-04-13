f57b9c890ddb973ac0336a80183160e6b44e0f3a
ArthurWdnr <121061305+ArthurWdnr@users.noreply.github.com>
2023-04-13 23:08:41 +0200

    toast d'erreur coordonnee

--- app/src/main/java/com/example/solarsoluce/CoordonneeActivity.java
+++ app/src/main/java/com/example/solarsoluce/CoordonneeActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 public class CoordonneeActivity  extends Activity {
     EditText coordonneeX2;
     EditText coordonneeY2;
@@ -16,11 +18,30 @@ public class CoordonneeActivity  extends Activity {
         coordonneeY2 = findViewById(R.id.coordonneeY2);
         coordValide = findViewById(R.id.coordValide);
         coordValide.setOnClickListener(arg0 -> {
-            String message= coordonneeX2.getText().toString()+","+coordonneeY2.getText().toString();
-            Intent intent=new Intent();
-            intent.putExtra("MESSAGE",message);
-            setResult(2,intent);
-            finish();//finishing activity
+            String latitude= coordonneeX2.getText().toString();
+            String longitude= coordonneeY2.getText().toString();
+            boolean b = true;
+            try {
+                Float f = Float.parseFloat(latitude);
+                Float f1 = Float.parseFloat(longitude);
+            } catch (NumberFormatException e) {
+                b = false;
+            }
+            if(b == true) {
+                float i = Float.parseFloat(latitude);
+                float i2 = Float.parseFloat(longitude);
+                if (-90 <= i && 90 >= i && -90 <= i2 && 90 >= i2 ) {
+                    String message = coordonneeX2.getText().toString() + "," + coordonneeY2.getText().toString();
+                    Intent intent = new Intent();
+                    intent.putExtra("MESSAGE", message);
+                    setResult(2, intent);
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Votre latitude ou votre longitude ne respecte pas le Système géodésique WGS 84 (valeur comprise entre -90 et 90 inclus", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "Votre latitude ou votre longitude ne correspondent pas à des décimaux ", Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
