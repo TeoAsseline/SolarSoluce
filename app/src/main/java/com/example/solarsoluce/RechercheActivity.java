c8cd1d594e2b9d1dc123471adbfc5def62904898
ArthurWdnr <121061305+ArthurWdnr@users.noreply.github.com>
2023-04-13 21:18:01 +0200

    wip startActivityForResult

--- app/src/main/java/com/example/solarsoluce/RechercheActivity.java
+++ app/src/main/java/com/example/solarsoluce/RechercheActivity.java
@@ -6,11 +6,28 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RechercheActivity extends AppCompatActivity {
+
+    ActivityResultLauncher<Intent> activityResult = registerForActivityResult(
+            new ActivityResultContracts.startAcivityForResult(),
+            new ActivityResultCallback<ActivityResult>{
+                @Override
+                public void onActivityResult(activityResult result) {
+
+        }
+    }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +35,29 @@ public class RechercheActivity extends AppCompatActivity {
         //ELEMENT DE LANCEMENT
         Button accueil = findViewById(R.id.Retour);
         accueil.setOnClickListener(v -> {
-            Intent accueilpage = new Intent(RechercheActivity.this,MainActivity.class);
+            Intent accueilpage = new Intent(RechercheActivity.this, MainActivity.class);
             startActivity(accueilpage);
         });
+
+
+        ImageButton coordonnee = findViewById(R.id.envoyercoor);
+        coordonnee.setOnClickListener(v -> {
+            Intent intent = new Intent(RechercheActivity.this, CoordonneeActivity.class);
+            activityLauncher.launch
+
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        TextView rechercheRetour = findViewById(R.id.rechercheRetour);
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the request code is same as what is passed  here it is 2
+        if(requestCode==2)
+        {
+            String message=data.getStringExtra("MESSAGE");
+            rechercheRetour.setText(message);
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
